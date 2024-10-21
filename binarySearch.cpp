f93731f047960ce17d4ba9cbb280dd12ac2cba3c
Jessica Odette Guizado Hernández <A01643724@tec.mx>
2024-10-21 09:09:32 -0600

    initial commit

---
+++ binarySearch.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+#include <utility>
+#include <algorithm>
+using namespace std;
+ 
+typedef long double ld;
+typedef long long int lli;
+typedef pair<lli, lli> pii;
+typedef vector<lli> vi;
+#define endl '\n'
+#define fi first
+#define se second
+#define pb push_back
+#define sz(s) lli(s.size())
+#define all(s) begin(s), end(s)
+#define print(s) cout << s << endl;
+#define fore(i, a, b) for(lli i = (a), TT = (b); i < TT; ++i)
+#define _ ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define printVector(s) for(lli i = 0; i < ((s).size()); i++) {print(s[i])}
+
